2eafd97e34a73d7e922c4291e765b6c5df50ac8a
Arthur MICHEL <contact@arthurmichel.com>
2018-10-09 16:14:24 +0200

    ajout script sql

--- additionnalResources/generationTest.sql
+++ additionnalResources/generationTest.sql
@@ -84,4 +84,70 @@ INSERT INTO [dbo].[utilisateur]
            ,3)
 GO
 
+INSERT INTO [dbo].[test]
+           ([libelle]
+           ,[description]
+           ,[duree]
+           ,[seuil_haut]
+           ,[seuil_bas])
+     VALUES
+           ('Analyse et conception'
+           ,'Test des compétences en analyse et conception.'
+           ,120
+           ,14
+           ,10),
+		   ('PHP et Symfony'
+           ,'Test des compétences de développement en PHP et Symfony.'
+           ,90
+           ,14
+           ,10),
+		   ('Procédures stockées en PL-SQL'
+           ,'Test des compétences PL-SQL.'
+           ,90
+           ,14
+           ,10)
+GO
 
+INSERT INTO [dbo].[epreuve]
+           ([dateDebutValidite]
+           ,[dateFinValidite]
+           ,[tempsEcoule]
+           ,[etat]
+           ,[note_obtenu]
+           ,[niveau_obtenu]
+           ,[idTest]
+           ,[idUtilisateur])
+     VALUES
+           ('02/06/2018'
+           ,'05/12/2019'
+           ,NULL
+           ,'EA'
+           ,NULL
+           ,NULL
+           ,1
+           ,3),
+		   ('04/10/2015'
+           ,'26/12/2015'
+           ,110
+           ,'T'
+           ,13
+           ,'ECA'
+           ,1
+           ,1),
+		   ('24/02/2018'
+           ,'22/09/2019'
+           ,56
+           ,'T'
+           ,8
+           ,'A'
+           ,2
+           ,14)
+GO
+
+INSERT INTO [dbo].[theme]
+           ([libelle])
+     VALUES
+           ('Comprehension'),
+		   ('Logique'),
+		   ('Connaissances')
+GO
